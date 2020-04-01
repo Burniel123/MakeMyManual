dbe9b3ec9ef1944681049285f6c68da775a55395
Daniel Burton <dgburton12@gmail.com>
2020-04-01 19:31:27 +0100

    Started playing around with ScrollPane.

--- src/display/Main.java
+++ src/display/Main.java
@@ -1,23 +1,42 @@
 package display;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
+public class Main extends Application
+{
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) throws Exception
+    {
+        /*Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 300, 275));*/
+        final FlowPane modulesPane = new FlowPane(2,2);
+        final ScrollPane scrollableWindow = new ScrollPane(modulesPane);
+        scrollableWindow.setFitToHeight(true);
+        scrollableWindow.setFitToWidth(true);
+
+        for(int i = 0; i < 50; i++)
+        {
+            final Region sampleRegion = new Region();
+            sampleRegion.setStyle("-fx-background-color: red");
+            sampleRegion.setMinSize(20, 20);
+            modulesPane.getChildren().add(sampleRegion);
+        }
+
+        final Scene scene = new Scene(scrollableWindow);
+        primaryStage.setTitle("MakeMyManual");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module MakeMyManual
 {
     requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.base;
 
     opens display;
 }
