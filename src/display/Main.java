8f4cc271195b7d28abd6d07b7452931d7e76c533
Daniel Burton <dgburton12@gmail.com>
2020-04-02 09:54:05 +0100

    More playing around with basic frontend.
    Former-commit-id: 21ad0d41a8be4e83ff81a379d0a09d43a3ac2547

--- src/display/Main.java
+++ src/display/Main.java
@@ -1,11 +1,13 @@
 package display;
 
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import manual.InputIOException;
 import manual.ManualListReader;
@@ -14,6 +16,11 @@ import java.util.ArrayList;
 
 public class Main extends Application
 {
+    private final VBox MAIN_PANE = new VBox(3);
+    private final String DEFAULT_STYLE = "-fx-background-color: darkred";
+
+    private ArrayList<manual.Module> MODULES_AVAILABLE = new ArrayList<manual.Module>();
+
     @Override
     public void start(Stage primaryStage) throws Exception
     {
@@ -25,22 +32,38 @@ public class Main extends Application
         scrollableWindow.setFitToHeight(true);
         scrollableWindow.setFitToWidth(true);
 
-        for(int i = 0; i < 50; i++)
+        final HBox topMenu = new HBox(2);
+        final TextField searchBarInput = new TextField("Search for mods...");
+        final Button searchBarSubmit = new Button("Search!");
+        final Separator searchSeparator = new Separator();
+        searchSeparator.setOrientation(Orientation.VERTICAL);
+        final Button filterMenu = new Button("Filter");
+        final Button presetsButton = new Button("Presets");
+        topMenu.getChildren().addAll(searchBarInput, searchBarSubmit, searchSeparator,
+                filterMenu, presetsButton);
+
+        final HBox numSelectedBar = new HBox(2);
+        final Label numSelected = new Label("Modules Selected: 0/98");
+        numSelectedBar.getChildren().add(numSelected);
+        MAIN_PANE.getChildren().addAll(topMenu, numSelectedBar);
+
+        for(int i = 0; i < 200; i++)
         {
             final Region sampleRegion = new Region();
-            sampleRegion.setStyle("-fx-background-color: red");
+            sampleRegion.setStyle(DEFAULT_STYLE);
             sampleRegion.setMinSize(20, 20);
             modulesPane.getChildren().add(sampleRegion);
         }
 
-        final Scene scene = new Scene(scrollableWindow);
+        MAIN_PANE.getChildren().add(scrollableWindow);
+        final Scene scene = new Scene(MAIN_PANE);
         primaryStage.setTitle("MakeMyManual");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     /**
-     *
+     * Initialises the Application by reading the module list file.
      */
     @Override
     public void init()
@@ -48,15 +71,15 @@ public class Main extends Application
         ManualListReader reader = new ManualListReader();
         try
         {
-            ArrayList<manual.Module> modulesAvailable = reader.readModuleList();
-            Platform.runLater(new Runnable()
+            MODULES_AVAILABLE = reader.readModuleList();
+/*            Platform.runLater(new Runnable()
             {
                 @Override
                 public void run()
                 {
                     //Call a method to render the available mods on-screen.
                 }
-            });
+            });*/
         }
         catch(InputIOException e)
         {
